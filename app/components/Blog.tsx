aa1878aae8afea17fec89baf959dc115af6e5e63
anshnarula5 <anshnarula55@gmail.com>
2024-03-06 00:04:13 +0530

    fix style

--- app/components/Blog.tsx
+++ app/components/Blog.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Blog = ({ image, name, date, readTime, link }: any) => {
   return (
-    <a href={link} className='group flex flex-col gap-3 max-w-[19rem] hover:-translate-y-2 transition-all p-4'>
+    <a href={link} className='group flex flex-col gap-3 md:max-w-[19rem] hover:-translate-y-2 transition-all p-4'>
       <img src={image} />
       <h2 className='text-lg font-[700] text-slate-300 group-hover:text-cyan-200 transition-all'> {/* Add group-hover text color here */}
         {name}
